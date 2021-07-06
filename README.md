1c9bede1917fc270ea7958d6523868104683f236
tiki-stack <tyrik.nelson1@gmail.com>
2021-07-06 16:47:47 -0400

    add static generation

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # NEXT.js Blog
 
 Made from the starter template from [Learn Next.js](https://nextjs.org/learn).
+
+npm run dev
--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,43 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
+      {/* Keep the existing code here */}
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>
-          Hello, I'm Tyrik. I am a software engineer and a comedian. Learn more about my projects on{' '}
-          <a href="https://www.github.com/tiki-stack">Github</a>
+        <p>Hello, I'm Tyrik. I'm a software developer and comedian. You can learn more about my projects on {' '}
+          <a href="https://github.com/tiki-stack">Github</a>
         </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      </section>
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
